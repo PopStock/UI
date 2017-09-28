1d36dade90ae9ff9c07a91b221e731ac966239d3
lentusignavus <ignavus.ii@gmail.com>
2017-09-28 11:05:18 -0500

    Adds firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as Firebase from "firebase";
+import * as ENV from "./env";
+
+
+Firebase.initializeApp({
+  apiKey: ENV.firebase.apiKey,
+  authDomain: ENV.firebase.authDomain,
+  databaseURL: ENV.firebase.databaseURL,
+  projectId: ENV.firebase.projectId,
+  storageBucket: ENV.firebase.storageBucket,
+  messagingSenderId: ENV.firebase.messagingSenderId
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
