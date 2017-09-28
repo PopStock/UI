ddfb2674af1ef8b26ee62c121add264e48b5713b
lentusignavus <ignavus.ii@gmail.com>
2017-09-28 13:07:04 -0500

    Scopes login page to Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./login/login"
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import List, { ListItem, ListItemText } from 'material-ui/List';
 
 class App extends Component {
   render() {
@@ -11,11 +17,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <List>
+              <ListItem>
+                <Link to="/login">
+                  <ListItemText primary="Login"/>
+                </Link>
+              </ListItem>
+            </List>
+            <hr/>
 
-        <Login/>
+            <Route path="/login" component={Login}/>
+          </div>
+        </Router>
       </div>
     );
   }
